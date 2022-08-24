da59637e5463c30fcadf76de388e63da1de7812b
rolandopalermo <support@rolandopalermo.com>
2022-08-23 23:46:23 -0500

    first commit

---
+++ src/main/java/com/rolandopalermo/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.rolandopalermo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
+
+@RestController
+@RequestMapping("greetings")
+class TestController {
+
+    @GetMapping
+    public ResponseEntity<String> greetings() {
+        return ResponseEntity.ok("Hello world");
+    }
+
+}
